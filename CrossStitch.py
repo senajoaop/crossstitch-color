02b6a7032debb1f13bd1c80cdb4a859d0427c037
senajoaop <sena.joaopedro42@gmail.com>
2022-07-10 10:44:23 -0300

    Separation of colors

--- CrossStitch.py
+++ CrossStitch.py
@@ -1,14 +1,17 @@
 import numpy as np
 from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 import cv2
 import json
+import sys
+
 
 
 class CrossStitch:
     def __init__(self):
         with open("color_ref.json", 'r') as file:
-            colorRef = json.load(file)
+            self.colorRef = json.load(file)
 
 
     def load_image(self, fileName, bgColor=(0, 255, 0)):
@@ -19,11 +22,95 @@ class CrossStitch:
         mask = cv2.inRange(hsv, np.array([0, 0, 0]), np.array([1, 1, 1]))
         img[mask > 0] = bgColor
 
-        plt.figure()
-        plt.axis("off")
-        plt.imshow(img)
+        self.img = img
+        self.imgDim = img.reshape((img.shape[0] * img.shape[1], 3))
+
+        # plt.figure()
+        # plt.axis("off")
+        # plt.imshow(img)
+        # plt.show()
+
+
+    def centroid_histogram(self, nClusters):
+        clt = KMeans(n_clusters=nClusters)
+        clt.fit(self.imgDim)
+        # based on the number of pixels assigned to each cluster
+        # grab the number of different clusters and create a histogram
+        numLabels = np.arange(0, len(np.unique(clt.labels_)) + 1)
+        (hist, _) = np.histogram(clt.labels_, bins=numLabels)
+        # normalize the histogram, such that it sums to one
+        hist = hist.astype("float")
+        hist /= hist.sum()
+
+        self.clt = clt
+        self.hist = hist
+
+
+    def prepare_colors(self):
+        # initialize the bar chart representing the relative frequency
+        # of each of the colors
+        bar = np.zeros((50, 300, 3), dtype="uint8")
+        startX = 0
+        # loop over the percentage of each cluster and the color of
+        # each cluster
+
+        for (percent, color) in zip(self.hist, self.clt.cluster_centers_):
+            if (np.round(color) == np.array([0, 255, 0])).all():
+                pFact = percent
+
+        colors = []
+
+        for (percent, color) in zip(self.hist, self.clt.cluster_centers_):
+            if (np.round(color) != np.array([0, 255, 0])).all():
+                # plot the relative percentage of each cluster
+                # endX = startX + (percent * 300)
+                endX = startX + (percent * (1 + pFact / (1 - pFact)) * 300)
+                cv2.rectangle(bar, (int(startX), 0), (int(endX), 50),
+                            color.astype("uint8").tolist(), -1)
+                startX = endX
+
+                colors.append(color)
+
+        self.bar = bar
+        self.colors = colors
+
+
+    def prepare_threads(self):
+        idxRef = []
+        idxDict = "0"
+        for c in self.colors:
+            minVal = 255
+
+            for idxC in self.colorRef:
+                if np.mean(np.abs(c - self.colorRef[idxC]["RGB"])) <= minVal:
+                    minVal = np.mean(np.abs(c - self.colorRef[idxC]["RGB"]))
+                    idxDict = idxC
+
+            idxRef.append(idxDict)
+
+        print(idxRef)
+
+        fig, axs = plt.subplots(2, len(idxRef))
+        for i, item in enumerate(idxRef):
+            imgAnchor = mpimg.imread(
+                    f'C:\\Users\\SENA\\Downloads\\color_ref\\Anchor\\Anchor-{self.colorRef[item]["Anchor"]}.jpg')
+            imgDmc = mpimg.imread(
+                    f'C:\\Users\\SENA\\Downloads\\color_ref\\DMC\\117mc_e_{self.colorRef[item]["DMC"]}_swatch_150x150.jpg')
+
+            axs[0, i].imshow(imgAnchor)
+            axs[1, i].imshow(imgDmc)
+
         plt.show()
 
 
+
+    def make_plots(self):
+        pass
+
+
 if __name__=="__main__":
-    CrossStitch().load_image("c:\\users\\sena\\downloads\\jujuba_bg.png")
+    cs = CrossStitch()
+    cs.load_image("c:\\users\\sena\\downloads\\jujuba_bg.png")
+    cs.centroid_histogram(6)
+    cs.prepare_colors()
+    cs.prepare_threads()
